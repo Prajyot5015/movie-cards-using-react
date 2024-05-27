d10eafbb7883e4a2287dbfb5c7a4ca2a73096a6a
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-05-27 13:27:42 +0530

    added more movie  cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client"
 import MovieCard from "./MovieCards";
-
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -13,6 +13,20 @@ root.render(
                 releaseDate="2023-12-22"
                 director="Prashanth Neel"
                 url="https://1847884116.rsc.cdn77.org/tamil/home/salaar_1.jpg"
+            />
+            <MovieCard
+                title="Pushpa"
+                rating="7.6"
+                releaseDate="2021-12-17"
+                director="Sukumar"
+                url="https://stat2.bollywoodhungama.in/wp-content/uploads/2021/07/Pushpa-4.jpg"
+            />
+            <MovieCard
+                title="K.G.F 2"
+                rating="8.3"
+                releaseDate="2022-04-13"
+                director="Prashanth Neel"
+                url="https://images.fandango.com/ImageRenderer/820/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/227428/kgfchapter2-posterart.jpg"
             />
         </div>
     </>
